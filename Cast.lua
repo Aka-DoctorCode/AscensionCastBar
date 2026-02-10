a600a3534e7ce13735db3fbc0d59c432f3b87a59
Aka-DoctorCode <37858279+Aka-DoctorCode@users.noreply.github.com>
2026-02-10 02:12:48 -0400

    V37
    Fixed missing dependency of TOC

--- Cast.lua
+++ Cast.lua
@@ -2,7 +2,7 @@
 -- Project: AscensionCastBar
 -- Author: Aka-DoctorCode 
 -- File: Cast.lua
--- Version: 36
+-- Version: 37
 -------------------------------------------------------------------------------
 -- Copyright (c) 2025–2026 Aka-DoctorCode. All Rights Reserved.
 --
